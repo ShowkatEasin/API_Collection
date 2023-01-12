2c76bd34f7a431d391c539032615afefaa60993c
Showkat Easin <68397684+ShowkatEasin@users.noreply.github.com>
2023-01-12 19:38:07 +0600

    first commit

---
+++ tests/Feature/ApiEndpoindTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ApiEndpoindTest extends TestCase
+{
+    public function it_list_down_all_users()
+    {
+
+        $response = $this->get( uri: '/api/users');
+
+        $response->assertStatus(status: 200);
+
+        
+    }
+    public function it_shows_details_of_a_user_to_authenticated_user (){
+
+    }
+    public function It_doesnot_allow_to_access_publicly (){
+
+    }
+    public function it_return_404_if_no_record_found_with_the_given_id (){
+
+    }
+}
